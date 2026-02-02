bce69809b0598ff8393b84cb89c4acebb543c0e9
yash-khandelwal-codr <yash.khandelwal.codr@gmail.com>
2026-02-02 13:01:57 +0530

    basic server added

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,13 +1,28 @@
-import express from "express";
+import express, { json } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true });
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({ Message: "Hello World" });
+  res.json({ message: "Basic Server" });
 });
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("server is connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
